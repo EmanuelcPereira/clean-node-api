ff0535137e331c1984f7d51f358dcb716e17975c
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-08-26 16:56:56 -0300

    feat: add ExpressRouteAdapter

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -15,6 +15,7 @@ describe('SignUp Routes', () => {
     const accountCollection = MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
+
   test('should returns an account on success', async () => {
     await request(app)
       .post('/api/signup')
