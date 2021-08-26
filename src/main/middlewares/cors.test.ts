c4d970a029636b669d0a6cebc6041018bfda2934
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-08-26 14:35:05 -0300

    feat: ensure SignUp routes returns an account on success

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -10,5 +10,7 @@ describe('Cors middleware', () => {
     await request(app)
       .get('/test_cors')
       .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*')
   })
 })
