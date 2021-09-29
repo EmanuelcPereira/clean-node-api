536a964f1c82a97eeeafb4b49f528c0c47ebd1ea
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-09-29 10:02:48 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,9 +1,18 @@
 import { AddSurveyModel, AddSurveyRepository } from '../../../../data/usecases/add-survey/db-add-survey-protocols'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-survey-repository'
+import { SurveyModel } from '../../../../domain/models/survey'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(surveyData)
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys = await surveyCollection.find().toArray()
+
+    return surveys as SurveyModel[]
   }
 }
