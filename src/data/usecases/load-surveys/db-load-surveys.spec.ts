ec5514b744e4a9cf992228b7acdd639d5e12028c
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-09-29 09:39:10 -0300

    feat: ensure LbLoadSurveys throws if LoadSurveysRepository throws

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -60,4 +60,11 @@ describe('DbLoadSurveys', () => {
     const httpResponse = await sut.load()
     expect(httpResponse).toEqual(makeFakeSurveys())
   })
+
+  test('should throws if LoadSurveysRepository throw', async () => {
+    const { sut, loadSurveysRepositoryStub } = makeSut()
+    jest.spyOn(loadSurveysRepositoryStub, 'loadAll').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow()
+  })
 })
