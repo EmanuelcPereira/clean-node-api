9e2853147c93482ec9b3565112c589de48723e17
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-09-29 10:06:58 -0300

    test: ensure SurveyMongoRepository returns an empty list if there are no surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -64,5 +64,11 @@ describe('Account Mongo Repository', () => {
       expect(surveys[0].question).toBe('any_question')
       expect(surveys[1].question).toBe('other_question')
     })
+
+    test('should load an empty list', async () => {
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(0)
+    })
   })
 })
