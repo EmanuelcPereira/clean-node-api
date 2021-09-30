25ccebf9e0f75112ac858aa142be0e6aef022fc6
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-09-30 16:34:00 -0300

    fix: make routes config ignore map files

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -7,7 +7,7 @@ export default (app: Express): void => {
   app.use('/api', router)
   const filePath = path.join(__dirname, '/../routes')
   readdirSync(filePath).map(async file => {
-    if (!file.includes('.test.')) {
+    if (!file.includes('.test.') && !file.endsWith('.map')) {
       (await import(`../routes/${file}`)).default(router)
     }
   })
