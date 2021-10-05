273fa27fcf9cd0ced11e89ac2adbec6d4610f9cc
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-10-05 06:51:28 -0300

    test: ensure SurveyResultMongoRepository insert new survey result if there is no data accountId and surveyId

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -0,0 +1,76 @@
+import { SurveyResultMongoRepository } from './survey-result-mongo-repository'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { Collection, ObjectId } from 'mongodb'
+import { SurveyModel } from '@/domain/models/survey'
+import { AccountModel } from '@/domain/models/account'
+
+let surveyCollection: Collection
+let surveyResultsCollection: Collection
+let accountCollection: Collection
+
+const makeSut = (): SurveyResultMongoRepository => {
+  return new SurveyResultMongoRepository()
+}
+
+const makeSurvey = async (): Promise<SurveyModel> => {
+  const res = await surveyCollection.insertOne({
+    question: 'any_question',
+    answers: [{
+      image: 'any_image',
+      answer: 'any_answer'
+    },
+    {
+      answer: 'other_answer'
+    }],
+    date: new Date()
+  })
+
+  const { answers } = await surveyCollection.findOne({ _id: res.insertedId })
+
+  return answers
+}
+
+const makeAccount = async (): Promise<AccountModel> => {
+  const res = await accountCollection.insertOne({
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password'
+  })
+  return res && MongoHelper.map(res.insertedId)
+}
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+
+    surveyResultsCollection = await MongoHelper.getCollection('surveyResults')
+    await surveyResultsCollection.deleteMany({})
+
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('save()', () => {
+    test('should add a survey result if its new', async () => {
+      const survey = await makeSurvey()
+      const account = await makeAccount()
+      const sut = makeSut()
+      const surveyResult = await sut.save({
+        surveyId: new ObjectId(survey.id).toHexString(),
+        accountId: new ObjectId(account.id).toHexString(),
+        answer: survey[0].answer,
+        date: new Date()
+      })
+      expect(surveyResult).toBeTruthy()
+    })
+  })
+})
