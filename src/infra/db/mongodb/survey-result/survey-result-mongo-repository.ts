37b19a9ce7d4c86e8e1594bc40f53cd0cf7a3832
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-10-05 10:01:18 -0300

    feat: ensure SurveyResultMongoRepository update surveyResult if its not new

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -1,13 +1,13 @@
 import { SaveSurveyResultModel, SaveSurveyResultRepository, SurveyResultModel } from '@/data/usecases/save-survey-result/db-save-survey-result-protocols'
-import { ObjectId } from 'mongodb'
+// import { ObjectId } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
   async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
     const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
     const res = await surveyResultCollection.findOneAndUpdate({
-      surveyId: new ObjectId(data.surveyId),
-      accountId: new ObjectId(data.accountId)
+      surveyId: data.surveyId,
+      accountId: data.accountId
     }, {
       $set: {
         answer: data.answer,
@@ -16,7 +16,6 @@ export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
     }, {
       upsert: true
     })
-
-    return res && MongoHelper.map(res)
+    return res.value && MongoHelper.map(res.value)
   }
 }
