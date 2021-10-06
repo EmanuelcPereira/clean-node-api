e01cbe13fd351dbdca289a6fecf205ea2d0e366a
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-10-06 08:23:12 -0300

    feat: ensure save survey result route returns 403 in invalid token is provided

---
+++ src/main/routes/survey-result-routes.ts
@@ -0,0 +1,8 @@
+import { adaptRoute } from '@/main/adapters/express-route-adapter'
+import { auth } from '@/main/middlewares/auth'
+import { makeSaveSurveyResultController } from '@/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory'
+import { Router } from 'express'
+
+export default (router: Router): void => {
+  router.put('/surveys/:surveyId/results', auth, adaptRoute(makeSaveSurveyResultController()))
+}
