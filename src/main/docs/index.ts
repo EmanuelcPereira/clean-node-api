958efc3046f6540b2fff7c86980373f8a4e7a1df
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-10-11 08:22:52 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'Api do Curso do Mango para realizar enquetes entre programadores',
+    version: '1.0.0'
+  }
+}
