20b3bd00e361201302bf8b5b2fe029738509998d
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-10-13 16:04:29 -0300

    docs: update save survey result docs

--- src/main/docs/schemas.ts
+++ src/main/docs/schemas.ts
@@ -8,7 +8,8 @@ import {
   signUpParamsSchema,
   addSurveyParamsSchema,
   saveSurveyParamsSchema,
-  surveyResultParamsSchema
+  surveyResultParamsSchema,
+  surveyResultAnswerSchema
 } from './schemas/'
 
 export default {
@@ -21,5 +22,6 @@ export default {
   surveyAnswer: surveyAnswerSchema,
   addSurveyParams: addSurveyParamsSchema,
   saveSurveyParams: saveSurveyParamsSchema,
-  surveyResult: surveyResultParamsSchema
+  surveyResult: surveyResultParamsSchema,
+  surveyResultAnswer: surveyResultAnswerSchema
 }
