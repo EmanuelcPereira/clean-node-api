24a65e1d329e5f4de10bb52150b4df98f5e6db90
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-10-14 08:51:30 -0300

    feat: ensure DbLoadSurveyResult call LoadSurveyResultRepository with correct values

--- src/data/usecases/survey-result/save-survey-result/db-save-survey-result.ts
+++ src/data/usecases/survey-result/save-survey-result/db-save-survey-result.ts
@@ -1,8 +1,8 @@
 import { SaveSurveyResult, SaveSurveyResultParams, SurveyResultModel, SaveSurveyResultRepository } from './db-save-survey-result-protocols'
 
 export class DbSaveSurveyResult implements SaveSurveyResult {
-  constructor(private readonly saveSurveyResultRepository: SaveSurveyResultRepository) { }
-  async save(data: SaveSurveyResultParams): Promise<SurveyResultModel> {
+  constructor (private readonly saveSurveyResultRepository: SaveSurveyResultRepository) { }
+  async save (data: SaveSurveyResultParams): Promise<SurveyResultModel> {
     const saveSurveyResult = await this.saveSurveyResultRepository.save(data)
     return saveSurveyResult
   }
