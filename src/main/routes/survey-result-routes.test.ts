f43234bc1aaf6c4db5d6c5250e222d71d9dc030e
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-10-27 16:15:08 -0300

    test: ensure load survey route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -83,5 +83,25 @@ describe('Survey Result Routes', () => {
         .get('/api/surveys/any_id/results')
         .expect(403)
     })
+
+    test('Should return 200 on load survey result with a valid accessToken', async () => {
+      const accessToken = await makeAccessToken()
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        },
+        {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+      const id = res.insertedId.toHexString()
+      await request(app)
+        .get(`/api/surveys/${id}/results`)
+        .set('x-access-token', accessToken)
+        .expect(200)
+    })
   })
 })
