89fc80c6bc1d59b1361a6e38ed4cce05256b0f26
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-11-03 17:20:16 -0300

    refactor: change mockHasher to HasherSpy

--- src/data/test/mock-criptography.ts
+++ src/data/test/mock-criptography.ts
@@ -2,14 +2,16 @@ import { Hasher } from '@/data/protocols/criptography/hasher'
 import { Decrypter } from '@/data/protocols/criptography/decrypter'
 import { Encrypter } from '@/data/protocols/criptography/encrypter'
 import { HashComparer } from '@/data/protocols/criptography/hash-comparer'
+import faker from 'faker'
 
-export const mockHasher = (): Hasher => {
-  class HasherStub implements Hasher {
-    async hash (value: string): Promise<string> {
-      return Promise.resolve('hashed_password')
-    }
+export class HasherSpy implements Hasher {
+  plaintext: string
+  digest = faker.datatype.uuid()
+
+  async hash (plaintext: string): Promise<string> {
+    this.plaintext = plaintext
+    return this.digest
   }
-  return new HasherStub()
 }
 
 export const mockDecrypter = (): Decrypter => {
