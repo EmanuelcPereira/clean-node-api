317b5a3711de9ddb082a352b2575fa775eff81b0
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-11-04 20:55:15 -0300

    refactor: change fake value using faker package

--- src/domain/test/mock-survey-result.ts
+++ src/domain/test/mock-survey-result.ts
@@ -1,25 +1,26 @@
 import { SaveSurveyResultParams } from '@/domain/usecases/survey-result/save-survey-result'
 import { SurveyResultModel } from '@/domain/models/survey-result'
+import faker from 'faker'
 
 export const mockSaveSurveyResultParams = (): SaveSurveyResultParams => ({
-  accountId: 'any_accountId',
-  surveyId: 'any_surveyId',
-  answer: 'any_answer',
-  date: new Date()
+  accountId: faker.datatype.uuid(),
+  surveyId: faker.datatype.uuid(),
+  answer: faker.random.word(),
+  date: faker.date.recent()
 })
 
 export const mockSurveyResultModel = (): SurveyResultModel => ({
-  surveyId: 'any_id',
-  question: 'any_question',
+  surveyId: faker.datatype.uuid(),
+  question: faker.random.words(),
   answers: [{
-    answer: 'any_answer',
-    count: 0,
-    percent: 0
+    answer: faker.random.word(),
+    count: faker.datatype.number({ min: 0, max: 1000 }),
+    percent: faker.datatype.number({ min: 0, max: 100 })
   }, {
-    answer: 'other_answer',
-    image: 'any_image',
-    count: 0,
-    percent: 0
+    answer: faker.random.word(),
+    image: faker.image.imageUrl(),
+    count: faker.datatype.number({ min: 0, max: 1000 }),
+    percent: faker.datatype.number({ min: 0, max: 100 })
   }],
-  date: new Date()
+  date: faker.date.recent()
 })
