53ef3b3eb8cff317ff5f5309a0450df9ab38bde1
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-11-04 08:24:49 -0300

    refactor: change mockSaveSurveyResultRepository to SaveSurveyResultRepositorySpy

--- src/data/test/mock-db-survey-result.ts
+++ src/data/test/mock-db-survey-result.ts
@@ -1,11 +1,11 @@
 import { SaveSurveyResultRepository } from '@/data/usecases/survey-result/save-survey-result/db-save-survey-result-protocols'
 import { SaveSurveyResultParams } from '@/domain/usecases/survey-result/save-survey-result'
 
-export const mockSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
-  class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
-    async save (data: SaveSurveyResultParams): Promise<void> {
-      return Promise.resolve()
-    }
+export class SaveSurveyResultRepositorySpy implements SaveSurveyResultRepository {
+  data: SaveSurveyResultParams
+
+  async save (data: SaveSurveyResultParams): Promise<void> {
+    this.data = data
+    return Promise.resolve()
   }
-  return new SaveSurveyResultRepositoryStub()
 }
