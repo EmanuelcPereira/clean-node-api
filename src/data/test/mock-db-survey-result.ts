6ed222335fa1e4a2e736da783023588be7daa702
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-11-04 08:28:57 -0300

    frefactor: group ssave and load survey result methods in a same file

--- src/data/test/mock-db-survey-result.ts
+++ src/data/test/mock-db-survey-result.ts
@@ -1,5 +1,6 @@
-import { SaveSurveyResultRepository } from '@/data/usecases/survey-result/save-survey-result/db-save-survey-result-protocols'
+import { LoadSurveyResultRepository, SaveSurveyResultRepository, SurveyResultModel } from '@/data/usecases/survey-result/save-survey-result/db-save-survey-result-protocols'
 import { SaveSurveyResultParams } from '@/domain/usecases/survey-result/save-survey-result'
+import { mockSurveyResultModel } from '@/domain/test'
 
 export class SaveSurveyResultRepositorySpy implements SaveSurveyResultRepository {
   data: SaveSurveyResultParams
@@ -7,5 +8,15 @@ export class SaveSurveyResultRepositorySpy implements SaveSurveyResultRepository
   async save (data: SaveSurveyResultParams): Promise<void> {
     this.data = data
     return Promise.resolve()
+  }
+}
+
+export class LoadSurveyResultRepositorySpy implements LoadSurveyResultRepository {
+  surveyId: string
+  surveyResultModel = mockSurveyResultModel()
+
+  async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
+    this.surveyId = surveyId
+    return Promise.resolve(this.surveyResultModel)
   }
 }
