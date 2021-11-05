fb8b20314f235ceb54ab66b1dcb9dcf54cb188e4
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-11-05 14:20:24 -0300

    refactor: remove mockEmailValidator on SignUpValidation Factory

--- src/main/factories/controllers/login/signup/signup-validation-factory.spec.ts
+++ src/main/factories/controllers/login/signup/signup-validation-factory.spec.ts
@@ -1,17 +1,9 @@
 import { makeSignUpValidation } from './signup-validation-factory'
-import { Validation, EmailValidator } from '@/presentation/protocols'
+import { Validation } from '@/presentation/protocols'
 import { CompareFieldsValidation, EmailValidation, RequiredFieldValidation, ValidationComposite } from '@/validation/validators'
+import { EmailValidatorAdapter } from '@/infra/validators/email-validator-adapter'
 
 jest.mock('@/validation/validators/validation-composite')
-
-const makeEmailValidator = (): EmailValidator => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid (email: string): boolean {
-      return true
-    }
-  }
-  return new EmailValidatorStub()
-}
 
 describe('SignUpValidation Factory', () => {
   test('should call ValidationComposite with all verifications', () => {
@@ -21,7 +13,7 @@ describe('SignUpValidation Factory', () => {
       validations.push(new RequiredFieldValidation(field))
     }
     validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
-    validations.push(new EmailValidation('email', makeEmailValidator()))
+    validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
