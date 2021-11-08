5d45ef29ca24ae50393b3fd3a7d0f5e5830f5424
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-11-08 06:50:23 -0300

    refactor: change mockValidation to ValidationSpy

--- src/presentation/test/mock-validation.ts
+++ src/presentation/test/mock-validation.ts
@@ -1,10 +1,11 @@
 import { Validation } from '@/presentation/protocols'
 
-export const mockValidation = (): Validation => {
-  class ValidationStub implements Validation {
-    validate (input: any): Error {
-      return null
-    }
+export class ValidationSpy implements Validation {
+  input: any
+  error: Error = null
+
+  validate (input: any): Error {
+    this.input = input
+    return this.error
   }
-  return new ValidationStub()
 }
